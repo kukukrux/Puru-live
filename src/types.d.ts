3f871e9d1b118ff6b89233361234cfd123e1aa1e
Mari Chiru <97845516+kukukrux@users.noreply.github.com>
2022-10-18 08:23:51 +0200

    Delete src directory

--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction, Collection, PermissionResolvable, Message } from "discord.js"
-
-
-export interface BotEvent {
-    name: string,
-    once?: boolean | false,
-    execute: (...args?) => void
-}
