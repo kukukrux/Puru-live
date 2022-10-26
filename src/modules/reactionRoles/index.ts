42398c447c51c17c8b719e36ce188784200039b5
Mari Chiru <mail@kukrux.com>
2022-10-26 11:39:31 +0200

    role KEK

--- src/modules/reactionRoles/index.ts
+++ src/modules/reactionRoles/index.ts
@@ -4,14 +4,39 @@ import { ReactionRoleConfiguration } from 'discordjs-reaction-role';
 
 const configuration: ReactionRoleConfiguration[] = [
 	{
-		messageId: '1030796134848933959',
-		reaction: '955914251447468032', // :white_check_mark:
-		roleId: '961763904285978684',
+		messageId: '1027272157635543040',
+		reaction: '1027269929835188416', // :a:red_flame:
+		roleId: '1027263363694211154', // RED
 	},
 	{
-		messageId: '1030796134848933959',
-		reaction: '955905602545148014', // :white_check_mark:
-		roleId: '615295761919508540',
+		messageId: '1027272157635543040',
+		reaction: '1027270197280776243', // :a:purple_gameboy:
+		roleId: '1027263610482864158',
+	},
+	{
+		messageId: '1027272157635543040',
+		reaction: '1027270428768616548', // :a:pink_flame:
+		roleId: '1027263581043048518',
+	},
+	{
+		messageId: '1027272157635543040',
+		reaction: '1027270563372204082', // :a:teal_gameboy:
+		roleId: '1027263423353987083',
+	},
+	{
+		messageId: '1027272157635543040',
+		reaction: '1027270990293647430', // :a:blue_flame:
+		roleId: '1027263545253056563',
+	},
+	{
+		messageId: '1027272157635543040',
+		reaction: '1027271193960652990', // :a:green_gameboy:
+		roleId: '1027265041470345216',
+	},
+	{
+		messageId: '1027272157635543040',
+		reaction: '1027271318166585384', // :a:black_flame:
+		roleId: '1027263639448723538',
 	},
 ];
 
