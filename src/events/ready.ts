502204e5485063ead045e8717bbafbb0da5e08c8
Mari Chiru <mail@kukrux.com>
2022-10-30 20:04:35 +0100

    HEHEHEHEHEHE

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -10,7 +10,6 @@ const event : BotEvent = {
 		const date = new Date();
 		const diff = date.getTime() - currentDate.getTime();
 		try {
-			// puruBotLogChannel.send(`IM ONLINE YAAYYY >W< <@930517933573165056> <@930512531569655808>\nAPI Latency is ${Math.round(client.ws.ping)}ms\nStartup at: ${currentDate.toLocaleString()} `);
 			logStuff(`IM ONLINE YAAYYY >W< <@930517933573165056> <@930512531569655808>\nAPI Latency is ${Math.round(client.ws.ping)}ms\nStartup at: ${currentDate.toLocaleString()} `);
 			console.log(`Ready! Logged in as ${client.user?.tag}. Startup took ${diff}ms. Startup at ${currentDate.toLocaleString()}`);
 		}
